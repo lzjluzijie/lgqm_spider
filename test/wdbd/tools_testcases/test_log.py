b4d2ea88e7e317a9970faea6683a7d52bf8c3987
shwdbd <shwangjj@163.com>
2019-08-30 08:44:02 +0800

    增加 单元测试 的框架

--- test/wdbd/tools_testcases/test_log.py
+++ test/wdbd/tools_testcases/test_log.py
@@ -7,6 +7,7 @@ class Test_DateUtils_get_dates(unittest.TestCase):
     def test_log(self):
         # tl.logger().info("这是 INFO 日志")
         print('fafdsa222')
+        self.assertFalse(True)
 
 
 
